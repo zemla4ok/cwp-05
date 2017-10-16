2719da7ab11d5d2d9046098256d4a5fddca036be
zemla4ok <dima-vital@live.com>
2017-10-16 15:57:01 +0300

    task 2

--- index.js
+++ index.js
@@ -4,11 +4,29 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+    parseBodyJson(req, (err, payload) => {
+        const c = { c: payload.a + payload.b };
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end( JSON.stringify(c) );
+    });
 });
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+function parseBodyJson(req, cb) {
+    let body = [];
+
+    req.on('data', function(chunk) {
+        body.push(chunk);
+    }).on('end', function() {
+        body = Buffer.concat(body).toString();
+
+        let params = JSON.parse(body);
+
+        cb(null, params);
+    });
+}
