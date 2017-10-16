e1f9a081e15b3a003d7c76126628a5c5dcca69f0
zemla4ok <dima-vital@live.com>
2017-10-16 18:05:28 +0300

    added logger

--- index.js
+++ index.js
@@ -4,44 +4,54 @@ const articles = require("./articles.json");
 const hostname = '127.0.0.1';
 const port = 3000;
 
+const handlers = {
+    'api/articles/readall': readAll()
+}
 
 const server = http.createServer((req, res) => {
-    parseBodyJson(req, (err, payload)=>{
+    parseBodyJson(req, (err, payload) => {
         console.log(articles);
         const handler = getHandler(req.url);
-        handler(req, res, payload, (err, result)=>{
-            if(err){
-                res.statusCode=err.code;
+        handler(req, res, payload, (err, result) => {
+            if (err) {
+                res.statusCode = err.code;
                 res.setHeader('Content-Type', 'application/json');
                 res.end(JSON.stringify(err));
                 return;
             }
-            res.statusCode=200;
+            res.statusCode = 200;
             res.setHeader('Content-Type', 'application/json');
             changeArticles();
             res.end(JSON.stringify(result));
         })
     })
 });
+
 function parseBodyJson(rec, cb) {
-    let body=[];
-    req.on('data',(chunk)=>{
+    let body = [];
+    req.on('data', (chunk) => {
         body.push(chunk);
-    }).on('end', ()=>{
+    }).on('end', () => {
         body = Buffer.concat(body).toString();
         let params;
-        if(body!==""){
+        if (body !== "") {
             params = JSON.parse(body);
         }
         cb(null, params);
     })
 }
+
 function changeArticles() {
     const file = fs.createWriteStream('articles.json');
     file.write(JSON.stringify(articles));
 }
-function getHandler(url) {
 
+function getHandler(url) {
+    return handlers[url] || notFound();
+}
+
+function notFound(req, res, payload, cb) {
+    cb({ code: 404, message: 'Not found'});
 }
 
 server.listen(port, hostname, () => {
