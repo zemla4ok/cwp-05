e57897b7dc994e0e683e095aa805ca46d7b10b5e
zemla4ok <dima-vital@live.com>
2017-10-16 17:35:52 +0300

    create articles.json

--- index.js
+++ index.js
@@ -1,60 +1,45 @@
 const http = require('http');
 
+const articles = require("./articles.json");
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const handlers = {
-    '/sum': sum
-};
 
 const server = http.createServer((req, res) => {
-    parseBodyJson(req, (err, payload) => {
+    parseBodyJson(req, (err, payload)=>{
+        console.log(articles);
         const handler = getHandler(req.url);
-
-        handler(req, res, payload, (err, result) => {
-            if (err) {
-                res.statusCode = err.code;
+        handler(req, res, payload, (err, result)=>{
+            if(err){
+                res.statusCode=err.code;
                 res.setHeader('Content-Type', 'application/json');
                 res.end(JSON.stringify(err));
-
                 return;
             }
-
-            res.statusCode = 200;
+            res.statusCode=200;
             res.setHeader('Content-Type', 'application/json');
+            ChangeArticles();
             res.end(JSON.stringify(result));
-        });
-    });
+        })
+    })
 });
+function parseBodyJson(rec, cb) {
+    let body=[];
+    req.on('data',(chunk)=>{
+        body.push(chunk);
+    }).on('end', ()=>{
+        body = Buffer.concat(body).toString();
+        let params;
+        if(body!==""){
+            params = JSON.parse(body);
+        }
+        cb(null, params);
+    })
+}
+function getHandler(url) {
+
+}
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-function getHandler(url) {
-    return handlers[url] || notFound;
-}
-
-function sum(req, res, payload, cb) {
-    const result = {c: payload.a + payload.b};
-
-    cb(null, result);
-}
-
-function notFound(req, res, payload, cb) {
-    cb({code: 404, message: 'Not found'});
-}
-
-function parseBodyJson(req, cb) {
-    let body = [];
-
-    req.on('data', function (chunk) {
-        body.push(chunk);
-    }).on('end', function () {
-        body = Buffer.concat(body).toString();
-
-        let params = JSON.parse(body);
-
-        cb(null, params);
-    });
-}
